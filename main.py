a72f7c1d306b9af5c6580c477f3a943cd99a5551
WesleyBumpus <wbumpus5@gmail.com>
2021-03-08 16:56:29 -0500

    Testing 1

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def print_hi(name):
 # Beep
 # bloop
 # blorp
-# blumbo
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Wesley, welcome to pycharm')
